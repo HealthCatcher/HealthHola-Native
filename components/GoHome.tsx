5d3ab8389179143ac0cbe3e7388e1563ae838fa6
BiYeon <isly7959@naver.com>
2024-06-26 18:18:04 +0900

    feat: 상단 홈 버튼 컴포넌트 생성

--- components/GoHome.tsx
+++ components/GoHome.tsx
@@ -1,11 +1,18 @@
 import React from "react";
-import { Link } from "expo-router";
-import { Button } from "react-native";
-import { useNavigation } from "@react-navigation/native";
+import {Button, Text, TouchableOpacity} from "react-native";
+import {useNavigation} from "@react-navigation/native";
+
 
 const GoHome = () => {
   const navigation = useNavigation();
-  return <Button title="Go to Home" onPress={() => navigation.navigate("index")} />;
+  return(
+      <TouchableOpacity
+          onPress={() => navigation.navigate('index')}
+          style={{marginLeft: 16}}
+      >
+        <Text style={{color: '#fff', fontWeight: 'bold'}}>홈</Text>
+      </TouchableOpacity>
+  );
 };
 
 export default GoHome;
