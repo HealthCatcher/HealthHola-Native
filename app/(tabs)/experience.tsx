e3697614d3d56b5aa02192325c5c2a39ecfbb243
BiYeon <isly7959@naver.com>
2024-06-26 18:18:29 +0900

    feat: 체험단 탭 페이지 구성

--- app/(tabs)/experience.tsx
+++ app/(tabs)/experience.tsx
@@ -1,11 +1,34 @@
-import { View, Text, StyleSheet } from "react-native";
+import {View, Text, StyleSheet} from "react-native";
+import {Stack} from "expo-router";
 import React from "react";
 
 export default function Experience() {
   return (
-    <View style={styles.container}>
-      <Text>Experience</Text>
-    </View>
+      <>
+        <Stack.Screen
+            options={{
+              title: "체험단",
+              headerStyle: {backgroundColor: "#f4511e"},
+              headerTintColor: "#fff",
+              headerTitleStyle: {
+                fontWeight: "bold",
+              },
+            }}
+        />
+          <View style={styles.category}>
+            <Text style={styles.text}>제품</Text>
+            <Text style={styles.text}>관심제품</Text>
+            <Text style={styles.text}>신청제품</Text>
+          </View>
+          <View style={styles.advertisement}>
+            <Text>광고</Text>
+          </View>
+          <View style={styles.product}>
+            제품
+          </View>
+
+
+      </>
   );
 }
 
@@ -14,5 +37,27 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+  },
+  category: {
+    height: 60,
+    padding: 15,
+    backgroundColor: "darkslateblue",
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  text: {
+    color: "#fff",
+    fontSize: 23,
+    textAlign: "center",
+  },
+  advertisement: {
+    height: 60,
+    padding: 15,
+    backgroundColor: "darkslateblue",
+  },
+  product: {
+    height: 60,
+    padding: 15,
+    backgroundColor: "darkslateblue",
   },
 });
